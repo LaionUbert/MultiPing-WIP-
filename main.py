dd9eb4e8fe7336157038675e1ff39302be2a3606
LaionUbert <laiondevubert@gmail.com>
2024-09-02 15:47:33 -0300

    First Commit

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-from pythonping import ping
-ping('192.168.4.62', verbose=True)
+import subprocess
+import os
+with open(os.devnull, "wb") as limbo:
+        subnet = 4
+        for n in range(0, 255):
+                ip="192.168.{}.{}".format(subnet, n)
+                result=subprocess.Popen(["ping", "-n", "1", "-w", "200", ip],
+                        stdout=limbo, stderr=limbo).wait()
+                if result:
+                        print (ip, "Down")
+                else:
+                        print (ip, "Up")
